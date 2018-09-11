f7b1e5860f1beeb8cb08cf719df867cbf169a02c
Nick Matthews <nick@kizzyco.com>
2018-09-11 14:28:26 +0100

    Add helpful batch files to remake website from webmob copy.

---
+++ webutil/make-from-webmod-debug.bat
@@ -0,0 +1,11 @@
+@echo off
+
+echo Run WebPageLayout
+
+xcopy /Q /S /I /Y ..\webmod ..\web-new
+
+..\build\msvc\debug\wpl --version
+
+..\build\msvc\debug\wpl --source ../webmod --target ../ --layout layout.json
+
+pause
